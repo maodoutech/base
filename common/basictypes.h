21215a3707f4e6fa3b50501ba4e49447bdbe0abb
zhangshilong <zhangshilong@baidu.com>
2016-11-16 10:36:07 +0800

    fix

--- common/basictypes.h
+++ common/basictypes.h
@@ -1,4 +1,4 @@
-// Copyright 2013 The Chromium Authors. All rights reserved.
+// Copyright 2016 The maodoutech Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,14 +8,12 @@
 // Note that the macros and macro-like constructs that were formerly defined in
 // this file are now available separately in base/macros.h.
 
-#ifndef BASE_BASICTYPES_H_
-#define BASE_BASICTYPES_H_
+#ifndef BASE_COMMON_BASICTYPES_H_
+#define BASE_COMMON_BASICTYPES_H_
 
 #include <limits.h>  // So we can set the bounds of our types.
 #include <stddef.h>  // For size_t.
 #include <stdint.h>  // For intptr_t.
-
-#include "base/macros.h"
 
 // DEPRECATED: Please use std::numeric_limits (from <limits>) instead.
 const uint8_t  kuint8max  = (( uint8_t) 0xFF);
@@ -31,4 +29,4 @@ const  int32_t kint32max  = (( int32_t) 0x7FFFFFFF);
 const  int64_t kint64min  = (( int64_t) 0x8000000000000000LL);
 const  int64_t kint64max  = (( int64_t) 0x7FFFFFFFFFFFFFFFLL);
 
-#endif  // BASE_BASICTYPES_H_
+#endif  // BASE_COMMON_BASICTYPES_H_
